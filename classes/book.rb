79a42a18fd9099cc225e51bc2822a5a4d3cf462a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-14 19:46:02 +0530

    Create Book class methods

--- classes/book.rb
+++ classes/book.rb
@@ -2,9 +2,19 @@ require './classes/item'
 
 class Book < Item
 
-  def initialize(publisher, cover_state)
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  private 
+
+  def can_be_archived?
+    if super || cover_state == "bad"
+      return true
+    else false
+    end
   end
 
 end
