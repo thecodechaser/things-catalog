b282f63690e4ff7a6052f5afe1be4d23155c92e0
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-14 18:43:10 +0530

    Create main.rb file

--- classes/item.rb
+++ classes/item.rb
@@ -1,25 +1,24 @@
 class Item
-
   attr_reader :genre, :author, :lable, :publish_date, :name, :archived
 
-  def initialize(name, publish_date, archived: false)
+  def initialize(_name, publish_date, archived: false)
     @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = archived
   end
 
   def genre=(genre)
-    @genre= genre
+    @genre = genre
     @genre.items.push(self) unless @genre.items.include?(self)
   end
 
   def author=(author)
-    @author= author
+    @author = author
     @author.items.push(self) unless @author.items.include?(self)
   end
 
   def label=(label)
-    @label= label
+    @label = label
     @label.items.push(self) unless @label.items.include?(self)
   end
 
@@ -27,12 +26,11 @@ class Item
     @archived = true if can_be_archived?
   end
 
-  private 
+  private
 
   def can_be_archived?
     current_year = Time.new.year
     publish_year = Date.strptime(@publish_date, '%Y-%m-%d').strftime('%Y').to_i
     current_year - publish_year >= 10
   end
-
 end
---
+++ main.rb
@@ -0,0 +1,57 @@
+class Main
+  INPUT_MESSAGE = "Please select an option by number"
+def print_options
+  puts "1. List all books
+  2. List all music albums
+  3. List of games
+  4. List all genres
+  5. List all labels
+  6. List all authors
+  7. Add a book
+  8. Add a music album
+  9. Add a game
+  10. Exit"
+  puts INPUT_MESSAGE
+    option = gets.chomp.to_i
+    handle_input(option)
+    print_options
+end
+
+def handle_input(option)
+  case option
+  when 1
+
+  when 2
+
+  when 3
+
+  when 4
+
+  when 5
+
+  when 6
+
+  when 7
+
+  when 8
+
+  when 9
+
+  when 10
+    puts "Thank you for using things-catalog"
+    exit
+  else 
+    puts "Invalid option! Please try again."
+  end
+end
+
+  def main
+    puts "Welcome to out things-catalog app!"
+    print_options
+  end
+  
+end
+
+catalog = Main.new
+
+catalog.main
