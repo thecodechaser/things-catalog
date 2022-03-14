cfa14e4d8d973c3f155b6c5826dd78ee3963ee18
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-14 19:34:38 +0530

    Update Item class

--- classes/item.rb
+++ classes/item.rb
@@ -9,17 +9,17 @@ class Item
 
   def genre=(genre)
     @genre = genre
-    @genre.items.push(self) unless @genre.items.include?(self)
+    @genre.add_item(self) unless @genre.items.include?(self)
   end
 
   def author=(author)
     @author = author
-    @author.items.push(self) unless @author.items.include?(self)
+    @author.add_item(self) unless @author.items.include?(self)
   end
 
   def label=(label)
     @label = label
-    @label.items.push(self) unless @label.items.include?(self)
+    @label.add_item(self) unless @label.items.include?(self)
   end
 
   def move_to_archive
@@ -30,7 +30,6 @@ class Item
 
   def can_be_archived?
     current_year = Time.new.year
-    publish_year = Date.strptime(@publish_date, '%Y-%m-%d').strftime('%Y').to_i
-    current_year - publish_year >= 10
+    current_year - @publish_date[:year] > 10
   end
 end
