09966c19d1ca79a38c128d2bc52597ff3709e99a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-14 19:52:14 +0530

    Add test file for book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,20 +1,19 @@
 require './classes/item'
 
 class Book < Item
-
   def initialize(publisher, cover_state, publish_date)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
 
-  private 
+  private
 
   def can_be_archived?
-    if super || cover_state == "bad"
-      return true
-    else false
+    if super || cover_state == 'bad'
+      true
+    else
+      false
     end
   end
-
 end
