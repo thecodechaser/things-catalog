efd9868f889c757a21e632a1d361cc37d8bc60ab
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-14 20:05:53 +0530

    Fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,18 +2,18 @@ require 'date'
 require './classes/book'
 
 describe Book do
-  context "What testing the Book class" do
+  context 'What testing the Book class' do
     book = Book.new('publisher', 'bad', Date.parse('2022/3/14'))
 
-    it "should validate the book publisher" do
-      expect(book.publisher).to eq "publisher"
+    it 'should validate the book publisher' do
+      expect(book.publisher).to eq 'publisher'
     end
 
-    it "should validate the book cover state" do
-      expect(book.cover_state).to eq "bad"
+    it 'should validate the book cover state' do
+      expect(book.cover_state).to eq 'bad'
     end
 
-    it "should validate the book publish date" do
+    it 'should validate the book publish date' do
       expect(book.publish_date).to eq Date.parse('2022/3/14')
     end
   end
