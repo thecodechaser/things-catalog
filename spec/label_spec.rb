01d328e9a3a2df474d2f2f6af0ad3560100b7e71
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-14 20:52:11 +0530

    Add test for Label class

---
+++ spec/label_spec.rb
@@ -0,0 +1,23 @@
+require './classes/label'
+require './classes/book'
+require 'date'
+
+describe Label do
+  context "When testing the Label class" do
+    book = Book.new('publisher', 'bad', Date.parse('2022/3/14'))
+    label = Label.new("Book-label", "Yellow")
+
+    it "Should validate the label title" do
+      expect(label.title).to eq "Book-label"
+    end
+
+    it "Should validate the label title" do
+      expect(label.color).to eq "Yellow"
+    end
+
+    it "should add book item object to label items array" do
+    label.add_item(book)
+    expect(label.items[0]).to eq book
+  end
+  end
+end
