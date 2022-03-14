732a2b1b64f93cd67283ecb2d321e294d6cac4a5
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-14 20:45:29 +0530

    Add add_item method to label class

--- classes/label.rb
+++ classes/label.rb
@@ -1,10 +1,15 @@
 class Label
-
-  def initialize(title, color)
+  attr_accessor :title, :color
+  attr_reader :items, :include
+    def initialize(title, color)
     @id = Random.rand(1..100)
     @title = title
     @color = color
     @items = []
   end
-  
+
+  def add_item(item)
+    @items.push(item)
+    item.label = self
+  end
 end
