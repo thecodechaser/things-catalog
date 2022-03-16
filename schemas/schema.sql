b90d63e182518340681777afb505beede4210fa5
Mucyosoda <mucyosofa@gmail.com>
2022-03-16 09:46:46 +0100

    Create books table

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -32,3 +32,24 @@ CREATE TABLE authors (
     last_name VARCHAR(100),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE item (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE,
+    archived BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
+);
+
+CREATE TABLE books (
+    id  INT,
+    title VARCHAR(100),
+    publisher VARCHAR(100),
+    cover_state VARCHAR(100),
+    FOREIGN KEY(id) REFERENCES item(id)
+);
