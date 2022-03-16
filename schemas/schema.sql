1b2181f9c19a726dbe748f59b7ea83dd60202353
Mucyosoda <mucyosofa@gmail.com>
2022-03-16 09:45:39 +0100

    Create authors table

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -18,3 +18,17 @@ CREATE TABLE labels (
     color VARCHAR(100),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE games (
+    id  INT,
+    multiplayer BOOLEAN,
+    last_played_at DATE,
+    FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE authors (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    PRIMARY KEY(id)
+);
