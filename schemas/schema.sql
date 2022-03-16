2676b27fa01df016f9fe199fa9744a8f67579d4c
Mucyosoda <mucyosofa@gmail.com>
2022-03-16 09:41:36 +0100

    Create music-album table

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE catalog;
+CREATE TABLE genres (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE music_albums (
+    id  INT,
+    name VARCHAR(100),
+    on_spotify BOOLEAN,
+    FOREIGN KEY(id) REFERENCES item(id)
+);
+
